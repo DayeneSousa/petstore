eac050acf024827a0fdbccb878031805dd184877
Dayene Sousa <sousadayene@gmail.com>
2025-07-28 23:24:22 -0300

    pontos de melhoria

--- cypress/e2e/buscar-petstore.cy.js
+++ cypress/e2e/buscar-petstore.cy.js
@@ -8,7 +8,7 @@
 
   describe('Buscar o animal de estimação', () => {
     it('deve retornar o animal de estimação com ID ', () => {
-      cy.getHeaders(9222968140497181000).then((response) => {
+      cy.getHeaders(2).then((response) => {
       cy.getbuscarpet('buscar-petstore.json', response.body);
      
         expect(response.status).to.eq(200);
